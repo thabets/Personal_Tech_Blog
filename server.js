0db4a466c84e552458fb9af9fc4d22b5a97b9909
Sam Thabet <sallam.thabet@gmail.com>
2021-11-25 18:24:00 -0500

    got rid of seeds

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ const sess = {
 };
 
 app.use(session(sess));
+const helpers = require("./utils/helpers");
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
